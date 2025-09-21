cda9b35995dd1dc56231e58bf60158f3f0c82b75
kaustav1811 <ikaustavdedhia@gmail.com>
2025-09-21 13:52:33 +0530

    Create ClaudeRecipe.jsx

---
+++ src/components/ClaudeRecipe.jsx
@@ -0,0 +1,12 @@
+import ReactMarkdown from "react-markdown"
+
+export default function ClaudeRecipe(props) {
+    return (
+        <section className = "suggested-recipe-container" aria-live = "polite">
+            <h2>Our AI Chef Recommends: </h2>
+            <ReactMarkdown>
+                {props.recipe}
+            </ReactMarkdown>
+        </section>
+    )
+}
